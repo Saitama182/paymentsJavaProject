e3c89ba4923140f3dc268f4285fd7afb69628d14
Saitama182 <100026214+Saitama182@users.noreply.github.com>
2023-10-02 19:19:36 +0300

    Типы данных

--- student-project/src/SaveStudentOrder.java
+++ student-project/src/SaveStudentOrder.java
@@ -1,10 +1,28 @@
 public class SaveStudentOrder {
     public static void main(String[] args) {
-        saveStudentOrder();
+        StudentOrder so = new StudentOrder();
+        so.hFirstName ="Алексей";
+        so.hLastName ="Петров";
+        so.wFirstName ="Яна";
+        so.wLastName ="Петрова";
+
+        StudentOrder so1 = new StudentOrder();
+        so1.hFirstName ="Алексей";
+        so1.hLastName ="Иванов";
+        so1.wFirstName ="Яна";
+        so1.wLastName ="Иванова";
+
+        long ans = saveStudentOrder(so);
+        System.out.println(ans);
+        long ans1 = saveStudentOrder(so1);
+        System.out.println(ans1);
     }
 
-    static void saveStudentOrder() {
-        System.out.println("saveStudentOrder");
+    static long saveStudentOrder(StudentOrder studentOrder) {
+        long answer;
+        answer = 199;
+        System.out.println("saveStudentOrder:" + studentOrder.hLastName);
+        return answer;
     }
 
 }
