d339bc72d31da76134a34c6c0495838c1462e18d
Saitama182 <100026214+Saitama182@users.noreply.github.com>
2023-10-12 22:44:02 +0300

    Id 22

--- student-project/src/edu/javacourse/studentorder/SaveStudentOrder.java
+++ student-project/src/edu/javacourse/studentorder/SaveStudentOrder.java
@@ -3,6 +3,8 @@ package edu.javacourse.studentorder;
 import edu.javacourse.studentorder.domain.other.Adult;
 import edu.javacourse.studentorder.domain.StudentOrder;
 
+import java.time.LocalDate;
+
 public class SaveStudentOrder {
     public static void main(String[] args) {
         buildStudentOrder(10);
@@ -22,12 +24,33 @@ public class SaveStudentOrder {
     }
 
     public static StudentOrder buildStudentOrder(long id) {
-        StudentOrder so = new StudentOrder();
-        so.setStudentOrderId(id);
-        StudentOrder so1 = so;
-        printStudentOrder(so1);
+        // Муж
+        Adult husband = new Adult("Петров", "Виктор", "Сергеевич", LocalDate.of(1997, 8, 24));
+        husband.setPassportSeria("" + (1000 + id));
+        husband.setPassportNumber("" + (100000 + id));
+        husband.setIssueDate(LocalDate.of(2017, 9, 15));
+        husband.setIssueDepartment("Отдел милиции №" + id);
+        husband.setStudentId("" + (100000 + id));
+        husband.setAddress(address);
+        // Жена
+        Adult wife = new Adult("Петрова", "Вероника", "Алекссевна", LocalDate.of(1998, 3, 12));
+        wife.setPassportSeria("" + (2000 + id));
+        wife.setPassportNumber("" + (200000 + id));
+        wife.setIssueDate(LocalDate.of(2018, 4, 5));
+        wife.setIssueDepartment("Отдел милиции №" + id);
+        wife.setStudentId("" + (200000 + id));
+        wife.setAddress(address);
+        // Ребенок
+        Child child = new Child("Петрова", "Ирина", "Викторовна", LocalDate.of(2018, 6, 29));
+        child.setCertificateNumber("" + (300000 + id));
+        child.setIssueDate(LocalDate.of(2018, 7, 19));
+        child.setIssueDepartment("ОТдел ЗАГС №" + id);
+        child.setAddress(address);
 
-        Adult husband = new Adult("Delong", "Tom", "Blink182", null);
+        so.setHusband(husband);
+        so.setWife(wife);
+        so.setChild(child);
+
         return so;
     }
 
